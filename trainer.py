5e69fe207f188f0b2df293a09100969e6bccc373
EstebanKiito <esteban.ortega@uc.cl>
2025-02-15 21:04:12 +1300

    Added dict print func

--- trainer.py
+++ trainer.py
@@ -4,37 +4,104 @@
 # print("".join(katakana_chars))
 # katakana_chars = [chr(i) for i in range(0x30A0, 0x30FF)]
 
-katakana_dict = {
-    "ア": "a",  "イ": "i",  "ウ": "u",  "エ": "e",  "オ": "o",
-    "カ": "ka", "キ": "ki", "ク": "ku", "ケ": "ke", "コ": "ko",
-    "サ": "sa", "シ": "shi","ス": "su", "セ": "se", "ソ": "so",
-    "タ": "ta", "チ": "chi","ツ": "tsu","テ": "te", "ト": "to",
-    "ナ": "na", "ニ": "ni", "ヌ": "nu", "ネ": "ne", "ノ": "no",
-    "ハ": "ha", "ヒ": "hi", "フ": "fu", "ヘ": "he", "ホ": "ho",
-    "マ": "ma", "ミ": "mi", "ム": "mu", "メ": "me", "モ": "mo",
-    "ヤ": "ya", "ユ": "yu", "ヨ": "yo",
-    "ラ": "ra", "リ": "ri", "ル": "ru", "レ": "re", "ロ": "ro",
-    "ワ": "wa", "ヲ": "wo", "ン": "n",
-    "ガ": "ga", "ギ": "gi", "グ": "gu", "ゲ": "ge", "ゴ": "go",
-    "ザ": "za", "ジ": "ji", "ズ": "zu", "ゼ": "ze", "ゾ": "zo",
-    "ダ": "da", "ヂ": "ji", "ヅ": "zu", "デ": "de", "ド": "do",
-    "バ": "ba", "ビ": "bi", "ブ": "bu", "ベ": "be", "ボ": "bo",
-    "パ": "pa", "ピ": "pi", "プ": "pu", "ペ": "pe", "ポ": "po",
-    "キャ": "kya", "キュ": "kyu", "キョ": "kyo",
-    "シャ": "sha", "シュ": "shu", "ショ": "sho",
-    "チャ": "cha", "チュ": "chu", "チョ": "cho",
-    "ニャ": "nya", "ニュ": "nyu", "ニョ": "nyo",
-    "ヒャ": "hya", "ヒュ": "hyu", "ヒョ": "hyo",
-    "ミャ": "mya", "ミュ": "myu", "ミョ": "myo",
-    "リャ": "rya", "リュ": "ryu", "リョ": "ryo",
-    "ギャ": "gya", "ギュ": "gyu", "ギョ": "gyo",
-    "ジャ": "ja",  "ジュ": "ju",  "ジョ": "jo",
-    "ビャ": "bya", "ビュ": "byu", "ビョ": "byo",
-    "ピャ": "pya", "ピュ": "pyu", "ピョ": "pyo"
-}
+import time
+import random
+
+katakana_dict = [
+    {"ア": "a",  "イ": "i",  "ウ": "u",  "エ": "e",  "オ": "o"},
+    {"カ": "ka", "キ": "ki", "ク": "ku", "ケ": "ke", "コ": "ko"},
+    {"サ": "sa", "シ": "shi","ス": "su", "セ": "se", "ソ": "so"},
+    {"タ": "ta", "チ": "chi","ツ": "tsu","テ": "te", "ト": "to"},
+    {"ナ": "na", "ニ": "ni", "ヌ": "nu", "ネ": "ne", "ノ": "no"},
+    {"ハ": "ha", "ヒ": "hi", "フ": "fu", "ヘ": "he", "ホ": "ho"},
+    {"マ": "ma", "ミ": "mi", "ム": "mu", "メ": "me", "モ": "mo"},
+    {"ヤ": "ya", "ユ": "yu", "ヨ": "yo"},
+    {"ラ": "ra", "リ": "ri", "ル": "ru", "レ": "re", "ロ": "ro"},
+    {"ワ": "wa", "ヲ": "wo"},
+    {"ン": "n"},
+    {"ガ": "ga", "ギ": "gi", "グ": "gu", "ゲ": "ge", "ゴ": "go"},
+    {"ザ": "za", "ジ": "ji", "ズ": "zu", "ゼ": "ze", "ゾ": "zo"},
+    {"ダ": "da", "ヂ": "ji", "ヅ": "zu", "デ": "de", "ド": "do"},
+    {"バ": "ba", "ビ": "bi", "ブ": "bu", "ベ": "be", "ボ": "bo"},
+    {"パ": "pa", "ピ": "pi", "プ": "pu", "ペ": "pe", "ポ": "po"},
+    {"キャ": "kya", "キュ": "kyu", "キョ": "kyo"},
+    {"シャ": "sha", "シュ": "shu", "ショ": "sho"},
+    {"チャ": "cha", "チュ": "chu", "チョ": "cho"},
+    {"ニャ": "nya", "ニュ": "nyu", "ニョ": "nyo"},
+    {"ヒャ": "hya", "ヒュ": "hyu", "ヒョ": "hyo"},
+    {"ミャ": "mya", "ミュ": "myu", "ミョ": "myo"},
+    {"リャ": "rya", "リュ": "ryu", "リョ": "ryo"},
+    {"ギャ": "gya", "ギュ": "gyu", "ギョ": "gyo"},
+    {"ジャ": "ja",  "ジュ": "ju",  "ジョ": "jo"},
+    {"ビャ": "bya", "ビュ": "byu", "ビョ": "byo"},
+    {"ピャ": "pya", "ピュ": "pyu", "ピョ": "pyo"}
+]
+
+columns = ["A", "I", "U", "E", "O"]
+
+def character_practice():
+    return "Modo practica"
+
+def view_dict():
+    # for key,value in katakana_dict.items():
+    #     print(f"{key} -> {value}")
+
+    print("\n\t📜 Katakana Chart 📜")
+    print("──────────────────────────────────────")
+    print("       " + "      ".join(columns))  # Encabezado
+
+    for elem in katakana_dict:
+        # print(elem)
+        # --- Algoritmo a continuacion -> Mejorar: No se me ocurrio otra forma parcialmente
+        random_item = random.choice(list(elem.items())) # (llave, valor)
+        character = random_item[1][0]
+        
+        if character.upper() not in columns:
+            dict_tolist = list(elem.items())
+            print(f"{character.upper()}:", end=" ")
+            for item in dict_tolist:
+                print(f"{item[0]} ({item[1]})", end=" ")
+            print("\n")
+       
+        else:
+            dict_tolist = list(elem.items())
+            # print(dict_tolist)
+            # print(f"\t {}")
+            print("\n  ", end=" ")
+            for item in dict_tolist:
+                print(f"{item[0]} ({item[1]})", end=" ")
+            print("\n")
+
+    print()
+
+def exit_app():
+    print("\n👋 Exiting the app. See you soon!")
+    exit()
 
 def main():
-    print("Welcome to the Katakana Practice App! 🎌")
+    print(" 1️⃣  Start Practice")
+    print(" 2️⃣  View Katakana Dict")
+    print(" 3️⃣  Exit")
+    
+    # ---- Timer: Cuanto tiempo dedicaste a practicar! ----
+    timer = 0
+    
+    # ---- Main program: ----
+    mode = int(input("Select an option (1-3): "))
+    
+    if mode == 1:
+        character_practice()
+        # n_corrects = character_practice()
+        # print(f"🇯🇵 Numbers char correct: {n_corrects} 😈")
+    elif mode == 2:
+        view_dict()
+    elif mode == 3:
+        exit_app()
+    else:
+        print("\n❌ Invalid choice! Please select a valid option.\n")
+
 
 if __name__ == "__main__":
-    main()  
+    print(">> Welcome to the Katakana Practice App! 🎌")
+    while(True):
+        main()  
